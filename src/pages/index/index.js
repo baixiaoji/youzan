ed9ec84df076d6dff3cdb8923d6d6c9351d02235
Kevin <1041874421@qq.com>
2017-10-11 20:59:53 +0800

    [TODO] finish the infinite loading function

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -6,23 +6,40 @@ import Vue from 'vue'
 import axios from "axios"
 import url from "js/api.js"
 import foot from "components/foot.vue"
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll)
 new Vue({
     el:"#app",
     data:{
         lists:null,
         pageNum:1,
-        pageSize:6
+        pageSize:6,
+        loading:false,
+        allLoaded:false,
     },
     created(){
         this.getLists()
     },
     methods:{
         getLists(){
+            if(this.allLoaded) return //全部加载完就不加载
+            this.loading = true
             axios.post(url.hotLists,{
                 pageNum:this.pageNum,
                 pageSize:this.pageSize
             }).then(res=>{
-                this.lists = res.data.lists
+                let curLists = res.data.lists
+                // 判断当前条数是不是一致，不是便是尽头
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                //当前是否是第一次加载数据
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    this.lists = curLists
+                }
+                this.loading = false
             })
         }
     },
